477a3e19c2e3c1502f035fab36e81557654e9246
Awais64 <abdulhadi641164@gmail.com>
2023-10-08 19:20:35 +0500

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
+app.get("/images", (req, res) => {
   res.send("Hello from Space! 🚀");
 });
 
